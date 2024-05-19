041f1459c25411ab1fe777b3542eee84085587a7
Maggie Li <lixiaoqiscut@126.com>
2024-05-19 14:07:27 -0500

    twoSum with java solution

--- JA/array/TwoSum.java
+++ JA/array/TwoSum.java
@@ -1,4 +1,8 @@
 package JA.array;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 class TwoSum {
 //     Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
@@ -20,8 +24,46 @@ class TwoSum {
 
 // Input: nums = [3,3], target = 6
 // Output: [0,1]
- 
+    // brute force
+    public int[] twoSumBruteForce(int[] nums,int target){
+        int[] res =new int[2];
+        int l = nums.length;
+        for(int i=0;i<l;i++){
+            int a =nums[i];
+            int b = target-a;
+            for(int j=i+1;j<l;j++){
+                if( nums[j]==b){
+                    res[0]=i;
+                    res[1] =j;
+                }
+            }
+        }
+        return res;
+    }
+
+    // solution 2: hashmap with O(n)
+    public int[] twoSumHashMap(int[] nums,int target){
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int i=0;i< nums.length;i++){
+            int complement = target- nums[i];
+            if (map.containsKey(complement)){
+                return new int[]{map.get(complement),i};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException();
+    }
+
     public static void main (String[] args){
-        
+        // brute force 
+        TwoSum solution = new TwoSum();
+        int[] nums = {3,2,4};
+        int target = 6;
+        int[] res = solution.twoSumBruteForce(nums, target);
+        System.out.println(Arrays.toString(res));
+
+        // hashmap
+        int[] res2 = solution.twoSumHashMap(nums, target);
+        System.out.println(Arrays.toString(res2));
     }
 }
